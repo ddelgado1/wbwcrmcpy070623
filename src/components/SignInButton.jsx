7ef79a3acd6fb88c88e02918e3d6ddec9181a9b7
Zach Mines <zachmines@Zachs-iMac.local>
2022-10-15 15:43:42 -0400

    it works finally

--- src/components/SignInButton.jsx
+++ src/components/SignInButton.jsx
@@ -3,17 +3,19 @@ import { useMsal } from "@azure/msal-react";
 import { loginRequest } from "../authConfig";
 import Button from "react-bootstrap/Button";
 
+function handleLogin(instance) {
+    instance.loginRedirect(loginRequest).catch(e => {
+        console.error(e);
+    });
+}
+
+/**
+ * Renders a button which, when selected, will redirect the page to the login prompt
+ */
 export const SignInButton = () => {
     const { instance } = useMsal();
 
-    const handleLogin = (loginType) => {
-        if (loginType === "popup") {
-            instance.loginPopup(loginRequest).catch(e => {
-                console.log(e);
-            });
-        }
-    }
     return (
-        <Button variant="secondary" className="ml-auto" onClick={() => handleLogin("popup")}>Sign in using Popup</Button>
+        <Button variant="secondary" className="ml-auto" onClick={() => handleLogin(instance)}>Sign in using Redirect</Button>
     );
 }
